1f85a20f6c56eb9e5a580efe213d6289ae8e474a
Leonardo Fabricio <ceoleonardofabricio@gmail.com>
2026-01-29 21:08:33 -0300

    organiza frontend em pasta dedicada

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend',
+  publicDir: 'frontend/public',
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
 })
